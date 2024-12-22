c9941e162cc5cb0c0111eaa5d836509f7c0fd832
Rana Awais <awaiswork803@gmail.com>
2024-12-22 17:55:13 +0500

    focus ring

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
   safelist: [
     {
       pattern:
-        /(bg|border)-(primary|secondary|slate|gray|zinc|neutral|red|yellow|green)(-(50|100|200|300|400|500|600|700|800|900|950))?/,
+        /(bg|border|ring)-(primary|secondary|slate|gray|zinc|neutral|red|yellow|green)(-(50|100|200|300|400|500|600|700|800|900|950))?/,
       variants: ["hover", "focus", "hover:dark"],
     },
     {
